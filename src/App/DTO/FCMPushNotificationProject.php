cb763e51b7f8b22e3c6ecae44aae9258f34bf95d
Oleksandr Polosmak <olexandr.polosmak123@gmail.com>
2023-09-01 12:24:58 +0300

    Addd push notifications project configs

---
+++ src/App/DTO/FCMPushNotificationProject.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Description of FCMPushNotificationProject.php
+ * @copyright Copyright (c) DOTSPLATFORM, LLC
+ * @author    Oleksandr Polosmak <o.polosmak@dotsplatform.com>
+ */
+
+namespace Dotsplatform\Notifications\DTO;
+
+
+abstract class FCMPushNotificationProject
+{
+    public const PROJECT_COURIERS = 'couriers';
+    public const PROJECT_1 = 'connection1'; // misteram
+    public const PROJECT_2 = 'connection2'; // dotsplatform
+    public const PROJECT_3 = 'connection3'; // dotsplatform-2
+}
